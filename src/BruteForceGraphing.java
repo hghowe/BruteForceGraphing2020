a44aa6b19d5046585bab05798dd56c7e389cc5e2
Harlan Howe <hhowe7@gatech.edu>
2020-08-22 15:40:15 -0500

    Initial commit

---
+++ src/BruteForceGraphing.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BruteForceGraphing
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboardReader = new Scanner(System.in);
+
+        int range, domain, yIntercept;
+        double slope;
+
+        System.out.print("Please enter the range: ");
+        range = keyboardReader.nextInt();
+
+        System.out.print("Please enter the domain: ");
+        domain = keyboardReader.nextInt();
+
+        System.out.print("Please enter the y-intercept: ");
+        yIntercept = keyboardReader.nextInt();
+
+        System.out.print("Please enter the slope: ");
+        slope = keyboardReader.nextDouble();
+
+        //----------------------------------
+        // TODO: You write the rest!
+
+
+        //----------------------------------
+    }
+
+
+}
